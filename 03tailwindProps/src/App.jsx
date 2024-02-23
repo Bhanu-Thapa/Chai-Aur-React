6146f6ba94e9b8c6a6a9559fe1aef1b423a33b13
BhanuThapa <bhanuthapa252@gmail.com>
2024-02-24 01:37:06 +0530

    Passing Props and set default value

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css';
-
+import Card from './components/Card.jsx';
 function App() {
+  // const myObj = {
+  //   style: 'Rep',
+  //   age: 23,
+  // };
+
+  // const myArr = [1, 2, 3];
+
   return (
     <>
-      <h1 className="bg-green-400 text-black p-4 rounded-xl">Bhanu Thapa</h1>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Bhanu Thapa
+      </h1>
+      <Card myName="Bhanu" btnText="Click Me" />
+      <Card myName="Taddy" />
     </>
   );
 }
