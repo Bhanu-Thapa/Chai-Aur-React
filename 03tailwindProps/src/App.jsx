75662e6f25e22d28d8fa99f6ce328fa32f120bf2
BhanuThapa <bhanuthapa252@gmail.com>
2024-02-23 23:58:50 +0530

    update

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -4,28 +4,6 @@ function App() {
   return (
     <>
       <h1 className="bg-green-400 text-black p-4 rounded-xl">Bhanu Thapa</h1>
-      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-        <img
-          className="w-24 h-24 rounded-full mx-auto"
-          src="https://e1.pxfuel.com/desktop-wallpaper/564/162/desktop-wallpaper-24-about-tanjiro-cute-tanjiro-thumbnail.jpg"
-          alt=""
-          width="384"
-          height="512"
-        />
-        <div className="pt-6 space-y-4">
-          <blockquote>
-            <p className="text-lg font-medium">
-              “Tailwind CSS is the only framework that I've seen scale on large
-              teams. It’s easy to customize, adapts to any design, and the build
-              size is tiny.”
-            </p>
-          </blockquote>
-          <figcaption className="font-medium">
-            <div className="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
-            <div>Staff Engineer, Algolia</div>
-          </figcaption>
-        </div>
-      </figure>
     </>
   );
 }
