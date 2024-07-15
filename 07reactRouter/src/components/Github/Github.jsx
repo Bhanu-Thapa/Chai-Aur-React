adcb776ae508b89effeee394b8a28ed781af6f53
BhanuThapa <bhanuthapa252@gmail.com>
2024-07-15 23:58:41 +0530

    08miniContext Directory setup

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -3,15 +3,15 @@ import { useLoaderData } from 'react-router-dom';
 // import { useEffect, useState } from 'react';
 
 function Github() {
-  // const [data, setData] = useState();
-  // useEffect(() => {
-  //   fetch('https://api.github.com/users/Bhanu-Thapa')
-  //     .then((response) => response.json())
-  //     .then((datain) => {
-  //       console.log(datain);
-  //       setData(datain);
-  //     });
-  // }, []);
+  //   const [data, setData] = useState();
+  //   useEffect(() => {
+  //     fetch('https://api.github.com/users/Bhanu-Thapa')
+  //       .then((response) => response.json())
+  //       .then((datain) => {
+  //         console.log(datain);
+  //         setData(datain);
+  //       });
+  //   }, []);
 
   const data = useLoaderData();
 
@@ -19,9 +19,12 @@ function Github() {
     <>
       <div className="p-4 bg-green-500 flex gap-48 w-2/3 mx-auto">
         <img className="w-1/4" src={data.avatar_url} alt="" />
-        <h3 className="text-white text-center text-3xl">
-          Respository : {data.public_repos}
-        </h3>
+        <div className="mt-10">
+          <h1 className="text-4xl text-white mb-8">Name - {data.login}</h1>
+          <h3 className="text-white  text-3xl">
+            Respository : {data.public_repos}
+          </h3>
+        </div>
       </div>
     </>
   );
