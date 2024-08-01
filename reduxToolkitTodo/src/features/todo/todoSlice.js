2a0a26348baf574482c5e3112757fbb61315fa30
BhanuThapa <bhanuthapa252@gmail.com>
2024-08-01 23:55:25 +0530

    depth explanation in action, payload and reducer

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -22,3 +22,13 @@ export const todoSlice = createSlice({
     },
   },
 });
+
+// When you define reducers inside createSlice, it automatically generates action creators for you. These action creators are stored in todoSlice.actions.
+
+export const { addTodo } = todoSlice.actions;
+
+// createSlice also automatically generates the slice reducer based on the provided reducers and initial state. The reducer function is used to handle state transitions in the Redux store.
+// Reducer: A function that takes the current state and an action, and returns a new state. In your case, the reducer handles the addTodo action.
+// createSlice combines all the reducers you define into a single reducer function.
+// This combined reducer function is available via todoSlice.reducer.
+export default todoSlice.reducer;
