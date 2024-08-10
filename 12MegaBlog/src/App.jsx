d4600125b5c284cdf39c55b40576d85fb71f30c8
BhanuThapa <bhanuthapa252@gmail.com>
2024-08-11 01:06:22 +0530

    Checking user status with auth and updated the sotre, and lands the page by checking Condition

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
 
 function App() {
-  return (
-    <>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="text-green-600">
+      <Header />
       <h1>A blog app with appwrite</h1>
-    </>
-  );
+      <Footer />
+    </div>
+  ) : null;
 }
 
 export default App;
