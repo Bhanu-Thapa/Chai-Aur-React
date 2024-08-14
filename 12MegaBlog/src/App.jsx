2719124b2df0ede8ff2e640bb0bacfe2a5ec8c48
BhanuThapa <bhanuthapa252@gmail.com>
2024-08-14 22:58:44 +0530

    remove component folder

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,35 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import { useDispatch } from 'react-redux';
-import authService from './appwrite/auth';
-import { login, logout } from './store/authSlice';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    authService
-      .getCurrentUser()
-      .then((userData) => {
-        if (userData) {
-          dispatch(login({ userData }));
-        } else {
-          dispatch(logout());
-        }
-      })
-      .finally(() => setLoading(false));
-  }, []);
-
-  return !loading ? (
+  return (
     <div className="text-green-600">
-      <Header />
       <h1>A blog app with appwrite</h1>
-      <Footer />
     </div>
-  ) : null;
+  );
 }
 
 export default App;
