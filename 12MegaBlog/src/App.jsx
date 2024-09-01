9e26effa04a9d09ae8dbf54bce7f8f1fdb5f0b9a
BhanuThapa <bhanuthapa252@gmail.com>
2024-09-01 23:35:38 +0530

    Setup 12 Mega Blog (env)

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="text-green-600">
-      <h1>A blog app with appwrite</h1>
-    </div>
+    <>
+      <h1 className="text-green-600 ">Mega Blog</h1>
+    </>
   );
 }
 
