f667bef141a234e22a811e0acddfb864d9216df6
BhanuThapa <bhanuthapa252@gmail.com>
2024-09-06 13:40:50 +0530

    Conditional Rendering with Authentication (Login/Logout)

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import conf from './conf/conf';
+import Authservice from './appwrite/auth';
+import { useDispatch } from 'react-redux';
+import { login, logout } from './store/authSlice';
+
 function App() {
-  console.log(conf.appwriteUrl);
-  return (
-    <>
-      <h1 className="text-green-600 ">Mega Blog</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    Authservice.getCurrentUser()
+      .then((data) => {
+        if (data) {
+          dispatch(login(data));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return <>{loading ? null : <h1 className="text-green-600 ">Mega Blog</h1>}</>;
 }
 
 export default App;
